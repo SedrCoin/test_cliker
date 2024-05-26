3cf0b52151eaf59b1c556da41170162ce0b4028e
Artem <99964138+SedrCoin@users.noreply.github.com>
2024-05-26 23:56:37 +0300

    Update main.js
    add multytouch

--- main.js
+++ main.js
@@ -21,27 +21,27 @@ score.innerHTML = counter
 
 
 
-btn.addEventListener('click', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.querySelector('.button_clicker');
+    
+    button.addEventListener('touchstart', (event) => {
+        event.preventDefault();
+        for (let i = 0; i < event.touches.length; i++) {
+            createNumber(event.touches[i]);
+        }
+    });
+});
 
-    event.preventDefault();
-
-    counter++
-
-    score.innerHTML = counter
-
-
-    localStorage.setItem('counter', counter);
-
+function createNumber(touch) {
     const numberElement = document.createElement('div');
     numberElement.classList.add('number-animation');
     numberElement.innerText = "+1";
 
-    // Получаем координаты клика внутри кнопки
-    const button = event.currentTarget;
+    const button = touch.target;
     const rect = button.getBoundingClientRect();
-    const offsetX = event.clientX - rect.left;
-    const offsetY = event.clientY - rect.top;
-    
+    const offsetX = touch.clientX - rect.left;
+    const offsetY = touch.clientY - rect.top;
+
     // Добавляем случайное смещение
     const randomOffsetX = (Math.random() - 0.5) * 20; // Смещение по X в диапазоне [-10, 10] пикселей
     const randomOffsetY = (Math.random() - 0.5) * 20; // Смещение по Y в диапазоне [-10, 10] пикселей
@@ -49,20 +49,12 @@ btn.addEventListener('click', (event) => {
     numberElement.style.left = (offsetX + randomOffsetX) + "px";
     numberElement.style.top = (offsetY + randomOffsetY) + "px";
 
-    // Добавляем элемент в кнопку
     button.appendChild(numberElement);
 
-    // Удаляем элемент после завершения анимации
     setTimeout(() => {
         numberElement.remove();
     }, 1500);  // Убедитесь, что время тайм-аута совпадает с длительностью анимации
-
-})
-
-
-
-
-
+}
 
 
 
